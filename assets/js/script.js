3eab2f970d652d043a591946209431fd7b55a553
bw <badziobw@gmail.com>
2022-05-29 20:09:44 +0100

    added questions data set to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,83 @@
+/*10 sample questions to generate code, source: 
+https://www.sparknotes.com/film/lordoftherings/quiz/
+*/
+const quizQuestions = [{
+        Question: 'Who possesses the ring of power at the start of the trilogy?',
+        a: 'Gandalf',
+        b: 'Bilbo',
+        c: 'Frodo',
+        d: 'Sauron',
+        answer: 4
+    },
+    {
+        Question: 'Who do the hobbits encounter at the Prancing Pony? ',
+        a: 'Gandalf',
+        b: 'Legolas',
+        c: 'Strider',
+        d: 'Boromir',
+        answer: 3
+    },
+    {
+        Question: 'How many members make up the fellowship of the ring? ',
+        a: 'seven',
+        b: 'eight',
+        c: 'nine',
+        d: 'ten',
+        answer: 3
+
+    },
+    {
+        Question: 'What is the name of the elf in the fellowship?',
+        a: 'Gimli',
+        b: 'Legolas',
+        c: 'Merry',
+        d: 'Boromir',
+        answer: 2
+
+    },
+    {
+        Question: 'What is the name of the dwarf in the fellowship? ',
+        a: 'Gimli',
+        b: 'Legolas',
+        c: 'Merry',
+        d: 'Boromir',
+        answer: 1
+    },
+    {
+
+        Question: 'When the pass of Caradhras is blocked, where does Frodo say the fellowship should go? ',
+        a: 'Rivendell',
+        b: 'Lothlorien',
+        c: 'Moria',
+        d: 'Minas Tirith',
+        answer: 1
+
+    },
+    {
+        Question: 'Who is the first member of the fellowship to be separated from the group? ',
+        a: 'Merry and Pippin are carried off by orcs and Uruk-hai',
+        b: 'Boromir is killed',
+        c: 'Frodo and Sam leave the others behind',
+        d: 'All of the above',
+        answer: 4
+
+    },
+    {
+        Question: 'Who has been following Sam and Frodo? ',
+        a: 'Gollum',
+        b: 'Sméagol',
+        c: 'A and B',
+        d: 'None of the above',
+        answer: 3
+
+    },
+    {
+        Question: 'Who is the king of Rohan?',
+        a: 'Wormtongue',
+        b: 'Théoden',
+        c: 'Éowyn',
+        d: 'Éomer',
+        answer: 2
+
+    }
+]
